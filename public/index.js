de6e6594f5d6ff5b023dec7bd7328c546602d440
Emily Gleeson <engleeson@gmail.com>
2021-07-16 15:12:19 -0400

    everything is fixedgit add .

--- public/index.js
+++ public/index.js
@@ -1,6 +1,15 @@
 let transactions = [];
 let myChart;
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").then(reg => {
+      console.log("Real Service Worker!!", reg);
+    });
+  });
+}
+
+
 fetch("/api/transaction")
   .then(response => {
     return response.json();
--- public/service-worker.js
+++ public/service-worker.js
@@ -2,8 +2,8 @@ const FILES_TO_CACHE = [
   "/",
   "./index.html",
   "./index.js",
-  "./db/js",
-  "./css/styles.css",
+  "./db.js",
+  "./styles.css",
   "./manifest.webmanifest",
   "./icons/icon-192x192.png",
   "./icons/icon-512x512.png",
@@ -13,12 +13,7 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function (evt) {
-  
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-    );
-    
-  
+
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
@@ -45,7 +40,7 @@ self.addEventListener("activate", function(evt) {
 });
 
 self.addEventListener('fetch', function(evt) {
-  if(evt.request.url.includes('/api')) {
+  if(evt.request.url.includes('/api/')) {
       console.log("SERVICE WORKER FETCH DATA", evt.request.url);
   evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
@@ -65,11 +60,15 @@ self.addEventListener('fetch', function(evt) {
   }
 
   evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
-        });
-      })
-    );
-  });
-
+    fetch(evt.request).catch(function() {
+      return caches.match(evt.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
