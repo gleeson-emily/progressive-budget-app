29f1f6816f7691b607c8cde4dbc5fc9c551f71e3
Emily Gleeson <engleeson@gmail.com>
2021-07-19 14:44:16 -0400

    trying to fix server

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ router.post("/api/transaction", ({body}, res) => {
       res.json(dbTransaction);
     })
     .catch(err => {
-      res.status(404).json(err);
+      res.status(400).json(err);
     });
 });
 
@@ -18,7 +18,7 @@ router.post("/api/transaction/bulk", ({body}, res) => {
       res.json(dbTransaction);
     })
     .catch(err => {
-      res.status(404).json(err);
+      res.status(400).json(err);
     });
 });
 
@@ -28,7 +28,7 @@ router.get("/api/transaction", (req, res) => {
       res.json(dbTransaction);
     })
     .catch(err => {
-      res.status(404).json(err);
+      res.status(400).json(err);
     });
 });
 
